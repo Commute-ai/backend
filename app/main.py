c61d80eb1a3be1d576e84308c1f2ed2bae18188f
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-14 16:11:51 +0300

    feat(logger): add logging to the backend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import logging
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import app.utils.logger
 from app.api.v1.api import api_router
 from app.core.config import settings
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -17,6 +22,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+logger.info("Starting %s version v%s", settings.PROJECT_NAME, settings.VERSION)
 
 # Include API v1 router
 app.include_router(api_router, prefix=settings.API_V1_STR)
